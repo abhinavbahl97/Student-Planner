d886c49f8389c3059a960ca0da9a576edd5a3bd4
Abhinav Bahl <abhinavbahl97@gmail.com>
2019-05-07 14:38:33 -0400

    testCommit

---
+++ GroupProject/FitnessViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FitnessViewController.swift
+//  GroupProject
+//
+//  Created by abhinav bahl on 5/3/19.
+//  Copyright © 2019 Jonathan Kevin Rosales. All rights reserved.
+//
+
+import Foundation
